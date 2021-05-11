9c411b5c7e3d2fb28473d891971fd67a34c55cfa
StevenPan1234 <>
2021-05-11 06:36:42 +0000

    fix

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ let age = 0
 document.getElementById('button').addEventListener('click', movie)
 
 function movie () {
-  age = document.getElementById('input').value 
+  age = document.getElementById('input').value
   age = parseInt(age)
   if (age >= 17) {
-    document.getElementById('p').innerHTML = "R movie"
+    document.getElementById('p').innerHTML = 'R movie'
   }
+
   else if (age >= 13) {
-    document.getElementById('p').innerHTML = "PG-13 movie"
+    document.getElementById('p').innerHTML = 'PG-13 movie'
   }
+
   else {
-    document.getElementById('p').innerHTML = "G or PG movie"
+    document.getElementById('p').innerHTML = 'G or PG movie'
   }
 }
