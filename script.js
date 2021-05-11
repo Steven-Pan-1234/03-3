b6a62f88cc6b96aabf95a7d2734e8928bc925efe
StevenPan1234 <>
2021-05-11 06:50:49 +0000

    ixf

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function movie () {
   age = parseInt(age)
   if (age >= 17) {
     document.getElementById('p').innerHTML = 'R movie'
-    }
+  }
 
   else if (age >= 13) {
     document.getElementById('p').innerHTML = 'PG-13 movie'
-    }
+  }
 
   else {
     document.getElementById('p').innerHTML = 'G or PG movie'
-    }
+  }
 }
