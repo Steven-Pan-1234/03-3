ff3c089a2a52c55ddb528310571efeb8dad48767
StevenPan1234 <>
2021-05-11 05:21:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let age = 0
+
+document.getElementById('button').addEventListener('click', movie)
+
+function movie () {
+  age = document.getElementById('input').value 
+  age = parseInt(age)
+  if (age >= 17) {
+    document.getElementById('p').innerHTML = "R movie"
+  }
+  else if (age >= 13) {
+    document.getElementById('p').innerHTML = "PG-13 movie"
+  }
+  else {
+    document.getElementById('p').innerHTML = "G or PG movie"
+  }
+}
